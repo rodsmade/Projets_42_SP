8821bfc5a6bce843f93c45abdb8d9f7ba128506a
rodsmade <rodrigogregoldo@gmail.com>
2021-05-19 11:53:24 -0300

    Improved test file, isalnum, isalpha, isascii, isdigit DONE

--- Partie 1/Libft/ft_isalpha.c
+++
@@ -1,6 +0,0 @@
-int	ft_isalpha(char c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-			return (1);
-	return (0);
-}
--- Partie 1/Libft/tests/ok_ko.c
+++ Partie 1/Libft/tests/ok_ko.c
@@ -1,14 +1,26 @@
-#include<libft.h>
+#include<stdio.h>
+#include<ctype.h>
 
 int main()
 {
-	for (int i = 0; i < 256; i++)
+	int iterations = 256;
+	int all_ok = 0;
+	for (int i = 0; i < iterations; i++)
 	{
 		printf("i = %d: ", i);
-		if (isalnum(i) == ft_isalnum(i))
+		if (isdigit(i) == ft_isdigit(i))
+		{
 			printf("OK!\n");
+			all_ok++;
+		}
 		else
 			printf("KO! :(\n");
 	}
+	printf("\n\n-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_**\n");
+	printf("_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_\n\n");
+	if (all_ok == iterations)
+		printf("ALL OK ! ! ! Way to go.\n");
+	else
+		printf("Capotou ): see log above. %d\n", all_ok);
 	return 0;
 }
