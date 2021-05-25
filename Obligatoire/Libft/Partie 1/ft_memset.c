a2e9f5c15f0078f2a06e3fc527e422ab4469fd9d
Rods <rodrigogregoldo@gmail.com>
2021-05-24 22:21:20 -0300

    Setting the stage for all remaining functions in part 1

---
+++ Obligatoire/Libft/Partie 1/ft_memset.c
@@ -0,0 +1,3 @@
+void	*ft_memset(void *s, int c, size_t n);
+// The memset() function fills the first n bytes of the memory area pointed to by s with the constant byte c.
+// The memset() function returns a pointer to the memory area s.
--- Obligatoire/Libft/Partie 1/libft.h
+++ Obligatoire/Libft/Partie 1/libft.h
@@ -4,11 +4,18 @@
 # include<unistd.h>
 
 int		ft_atoi(const char *nptr);
+void	ft_bzero(void *s, size_t n);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
