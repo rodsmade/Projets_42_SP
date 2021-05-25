a46e3a13550bdcc9ada518227da68ee389270e81
Rods <rodrigogregoldo@gmail.com>
2021-05-24 21:39:44 -0300

    __backlog.txt update + removed commments from strlcpy

--- Obligatoire/Libft/Partie 1/OKft_strlcpy.c
+++ Obligatoire/Libft/Partie 1/OKft_strlcpy.c
@@ -19,21 +19,3 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dest_size)
 	dst[chars_written] = '\0';
 	return (ft_strlen(src));
 }
-
-#include <bsd/string.h>
-#include <stdio.h>
-
-int main()
-{
-	char *source1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *source2 = "abcdefghijklmnopqrstuvwxyz";
-	char dest1[26];
-	char dest2[26];
-	size_t dest_size = 229;
-	
-	printf("resultado1 (normal)    : %li\n", strlcpy(dest1, source1, dest_size));
-	printf("resultado2 (ft_strlcpy): %li\n", ft_strlcpy(dest2, source2, dest_size));
-	printf("dest1: %s\n", dest1);
-	printf("dest2: %s\n", dest2);
-	return 0;
-}
