c3da0e36020d789afc24c6dafde4087ba9038bde
Rods <rodrigogregoldo@gmail.com>
2021-05-24 22:07:02 -0300

    setting the stage for ft_strchr + ft_strrchr

--- Obligatoire/Libft/Partie 1/ft_strchr.c
+++ Obligatoire/Libft/Partie 1/ft_strchr.c
@@ -1,1 +1,3 @@
 char *ft_strchr(const char *s, int c);
+//The strchr() function returns a pointer to the first occurrence of the character c in the string s.
+// if not found returns NULL
---
+++ Obligatoire/Libft/Partie 1/ft_strrchr.c
@@ -0,0 +1,3 @@
+char	*strrchr(const char *s, int c);
+// The strrchr() function returns a pointer to the last occurrence of the character c in the string s.
+// if not found returns NULL
