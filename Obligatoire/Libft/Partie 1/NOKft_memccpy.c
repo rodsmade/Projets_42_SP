cc3c6c3e61c1b0deb816a69c68c1b2668057ebc8
Rods <rodrigogregoldo@gmail.com>
2021-05-26 21:28:57 -0300

    ft_memccpy DONE

---
+++ Obligatoire/Libft/Partie 1/NOKft_memccpy.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+// The memccpy() function copies no more than n bytes from memory area src to memory area dest, stopping when the character c is found.
+// The memccpy() function returns a pointer to the next character in dest after c, or NULL if c was not found in the first n characters of src.
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((char*) src)[i] != c)
+			((char *) dest)[i] = ((char*) src)[i];
+		else
+			return (dest + 1);
+		i++;
+	}
+	return (NULL);
+}
