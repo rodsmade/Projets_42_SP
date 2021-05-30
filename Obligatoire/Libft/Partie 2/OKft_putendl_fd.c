244472ca6cc8e138a0980d1de900670b79b8bb30
Rods <rodrigogregoldo@gmail.com>
2021-05-30 20:53:34 -0300

    ft_putendl_fd DONE

---
+++ Obligatoire/Libft/Partie 2/OKft_putendl_fd.c
@@ -0,0 +1,7 @@
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
--- Obligatoire/Libft/Partie 2/libft.h
+++ Obligatoire/Libft/Partie 2/libft.h
@@ -20,6 +20,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putstr_fd(char *s, int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
