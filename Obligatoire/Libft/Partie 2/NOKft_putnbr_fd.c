107aec717ab2a7c85e8ea59ce72db9ae3f01e5a7
Rods <rodrigogregoldo@gmail.com>
2021-06-02 12:39:40 -0300

    ft_putnbr_fd DONE but not so quite sth wrong being carried along

--- Obligatoire/Libft/Partie 2/NOKft_putnbr_fd.c
+++ Obligatoire/Libft/Partie 2/NOKft_putnbr_fd.c
@@ -4,4 +4,11 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	// vai precisar da itoa
 	// joga resultado itoa em ft_putstr_fd
+	ft_putstr_fd(ft_itoa(n), fd);
+}
+
+int main()
+{
+	ft_putnbr_fd(6854546, 1);
+	return (0);
 }
