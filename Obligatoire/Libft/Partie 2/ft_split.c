d75fbd892f77c7d5c0ca1ce284d84bdecc6c4ab9
Rods <rodrigogregoldo@gmail.com>
2021-06-02 22:23:43 -0300

    ft_split mid-flight, retomar o debug amanhã

--- Obligatoire/Libft/Partie 2/ft_split.c
+++ Obligatoire/Libft/Partie 2/ft_split.c
@@ -1,23 +1,30 @@
 #include "libft.h"
 
+size_t ft_strlen(const char *s);
+
 char	**ft_split(char const *s, char c)
 {
 	char	**halves;
+//	char	*half0;
+//	char	*half1;
 	int		cleft;
 
-	halves = malloc(2 * sizeof(char *));
+	halves = malloc(2 * sizeof(*halves));
 	//  find delimiter point
+	cleft = 0;
 	while (s[cleft] != c)
 		cleft++;
-	// joga s pro delimiter c, vai ser usado mais na frente
-	s += cleft;
 	// não engloba o delimiter c
+//	half0 = malloc(cleft * sizeof(char) + 1);
 	halves[0] = malloc(cleft * sizeof(char) + 1);
 	// engloba o delimiter c
+//	half1 = malloc((ft_strlen(s) - cleft) * sizeof(char) + 1);
 	halves[1] = malloc((ft_strlen(s) - cleft) * sizeof(char) + 1);
-	if (halves == NULL || halves[0] == NULL || halves[1] == NULL)
+	if (halves[0] == NULL || halves[1] == NULL)
 		return (NULL);
 	halves[0][cleft] = '\0';
+	// ATE AQUI TA DANDO BOM PARECE, RETOMAR A PARTIR DAQUI AMANHA
+	s += cleft;
 	while (--cleft >= 0)
 		halves[0][cleft] = s[cleft];
 	while (*s)
@@ -29,4 +36,22 @@ char	**ft_split(char const *s, char c)
 			halves[1][cleft + 1] = '\0';
 	}
 	return (halves);
+}
+
+size_t ft_strlen(const char *s)
+{
+	int qtd_caracteres;
+
+	qtd_caracteres = 0;
+	while (*s)
+	{
+		qtd_caracteres++;
+		s++;
+	}
+	return (qtd_caracteres);
+}
+
+int main(){
+	ft_split("and then the futhermost shake drove a murthering stake |in and cleft me right down to my center", '|');
+	return 0;
 }
