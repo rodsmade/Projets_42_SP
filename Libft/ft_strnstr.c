1adcecfe7f6ddb32c1835549f0e0fb754170f722
Rods <rodrigogregoldo@gmail.com>
2021-06-11 13:28:24 -0300

    ft_strnstr fixed

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -2,20 +2,21 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	unsigned int i = 0;
+	unsigned int i;
+	unsigned int j;
 
+	i = 0;
 	if (*needle == '\0')
 		return ((char *) haystack);
-	while (*haystack && i < len)
+	while (haystack[i] && i < len)
 	{
-		int j = 0;
-		while (haystack[i] == needle[j] && *needle)
+		j = 0;
+		while (haystack[i + j] == needle[j] && (i + j) < len) // e se acabar aqui o haystack?
 		{
-			i++;
 			j++;
 			if (needle[j] == '\0')
-				return ((char *) &(haystack[i - ft_strlen(needle)]));
-			else if (needle[j] != haystack[i] || haystack[i] == '\0')
+				return ((char *) &(haystack[i]));
+			else if (needle[j] != haystack[i + j] || haystack[i + j] == '\0')
 				break;
 		}
 		i++;
