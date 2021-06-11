bd7d4078a6e01f21372cf937616cf1ce345a1f02
Rods <rodrigogregoldo@gmail.com>
2021-06-11 12:32:24 -0300

    memccpy fixed

--- Libft/ft_memccpy.c
+++ Libft/ft_memccpy.c
@@ -9,8 +9,8 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		((char *) dest)[i] = ((char*) src)[i];
-		if (((char*) src)[i] == c)
+		((unsigned char *) dest)[i] = ((unsigned char*) src)[i];
+		if (((unsigned char*) src)[i] == (unsigned char) c)
 			return (dest + i + 1);
 		i++;
 	}
