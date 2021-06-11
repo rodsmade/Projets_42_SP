1446db3f887e0c42626c426f1923237970de1e13
Rods <rodrigogregoldo@gmail.com>
2021-06-11 13:06:41 -0300

    hfsfkhasfh

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -4,7 +4,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	unsigned int	i;
 
-	if (size = 0)
+	if (size == 0)
 		return (ft_strlen(src));
 	i = 0;
 	while (src[i] && i < (size - 1))
