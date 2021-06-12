506dde7660268b1b81de328292aebf1208db60e5
Rods <rodrigogregoldo@gmail.com>
2021-06-12 15:31:08 -0300

    itoa try again

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -23,42 +23,29 @@ static size_t count_digits(size_t n)
 	return (digits);
 }
 
-static long int	ft_power(int nb, int power)
-{
-	if (power < 0)
-		return (0);
-	if (power == 0)
-		return (1);
-	else
-		return (nb * ft_power(nb, power - 1));
-}
-
 char	*ft_itoa(int n)
 {
 	char			*result;
-	long int		big_n;
-	unsigned int	i;
+	unsigned int	negative;
+	long int		number;
+	unsigned int	digits;
 
-	// tratar quando n = 0 
-	if (n == 0)
-		return ("0\0");
-	big_n = n;
-	if (is_negative(n))
-		big_n *= -1;
-	// contar exatamente a qtd de dígitos p/ n alocar memória em excesso
-	result = malloc((is_negative(n) + count_digits(big_n) + 1) * sizeof(char));
+	negative = is_negative(n);
+	number = n;
+	if (negative == 1)
+		number *= -1;
+	digits = count_digits(number);
+	result = (char *)malloc(digits + negative + 1);
 	if (result == NULL)
 		return (NULL);
-	i = 0;
-	while (big_n > 0)
+	if (negative == 1)
+		result[0] = '-';
+	result[digits + negative] = '\0';
+	while (digits > 0)
 	{
-		// 2147483647
-		result[i] = (big_n / ft_power(10, (count_digits(big_n) - 1))) + '0';
-		big_n = big_n - (result[i] - '0') * ft_power(10, (count_digits(big_n) - 1));
-		i++;
+		result[(digits - 1) + negative] = (number % 10) + '0';
+		number /= 10;
+		digits--;
 	}
-	result[i] = '\0';
-	if (is_negative(n))
-		result = ft_strjoin("-", result);
 	return (result);
 }
