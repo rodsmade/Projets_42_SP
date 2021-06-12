87c0c96cc07324665a73c571928132202c11616d
Rods <49699403+rodsmade@users.noreply.github.com>
2021-06-12 15:21:18 -0300

    ITOA test

--- Obligatoire/Libft/tests/ok_ko.c
+++ Obligatoire/Libft/tests/ok_ko.c
@@ -352,3 +352,19 @@ int main()
 	printf("Resultado > %s\n", ft_strjoin(ft_strjoin(string1, string2), string3));
 	return 0;
 }
+
+// ITOA
+
+#include <stdio.h>
+int    main(void)
+{
+    printf("%s\n", ft_itoa(1));
+    printf("%s\n", ft_itoa(15454));
+    printf("%s\n", ft_itoa(2));
+    printf("%s\n", ft_itoa(123123));
+    printf("%s\n", ft_itoa(2147483647));
+    printf("%s\n", ft_itoa(-123123));
+    printf("%s\n", ft_itoa(-2147483647));
+    printf("%s\n", ft_itoa(-2147483648));
+    return (0);
+}
