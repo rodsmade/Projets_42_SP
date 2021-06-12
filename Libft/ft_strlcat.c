b3c374877ac2b58cf523e82ebb07f4868624cbfc
Rods <rodrigogregoldo@gmail.com>
2021-06-12 11:36:51 -0300

    strlcat DONE and TESTED

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -23,26 +23,3 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	}
 	return (dstlen + srclen);
 }
-
-#include <stdio.h>
-#include <bsd/string.h>
-int	main(void)
-{
-	char destcat[] = "largatixa";
-	char srccat[] = "malasiatica";
-	char destcat1[] = "largatixa";
-	char srccat1[] = "malasiatica";
-	printf("dest: %ld   src: %ld\n", ft_strlen(destcat), ft_strlen(srccat));
-	int retorno = ft_strlcat(destcat, srccat, 1);
-	int retorno1 = strlcat(destcat1, srccat1, 1);
-
-
-	
-
-	printf("dest: %s ... retorno: %d\n", destcat, retorno);
-	printf("final: %ld\n", ft_strlen(destcat));
-
-	printf(" OOOOP dest: %s ... retorno: %d\n", destcat1, retorno1);
-	printf("O OO OO Ofinal: %ld\n", ft_strlen(destcat1));
-	return (0);
-}
