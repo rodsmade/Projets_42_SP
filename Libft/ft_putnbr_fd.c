6de9b6c538f5b29f3da26628dc9343db1ff48200
Rods <rodrigogregoldo@gmail.com>
2021-06-12 16:04:16 -0300

    ft_putnbr_fd REVVD

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -2,6 +2,17 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	// joga resultado itoa em ft_putstr_fd
-	ft_putstr_fd(ft_itoa(n), fd);
+	unsigned int	number;
+
+	number = n;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		number = n * -1;
+	}
+	if (number > 9)
+	{
+		ft_putnbr_fd(number / 10, fd);
+	}
+	ft_putchar_fd(number % 10 + '0', fd);
 }
