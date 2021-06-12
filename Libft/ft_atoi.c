5e8791b5b7b32312b6ff7d27c68fe027c0dba13d
Rods <rodrigogregoldo@gmail.com>
2021-06-12 11:42:49 -0300

    testing atoi in guaca

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -9,15 +9,13 @@ int	ft_atoi(const char *nptr)
 	signal = 1;
 	number = 0;
 	i = 0;
-	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ' || nptr[i] == '+')
+	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ')
 		i++;
-	if ((nptr[i] == '-' && ft_isdigit(nptr[i + 1])) || ft_isdigit(nptr[i]))
+	if (((nptr[i] == '-' || nptr[i] == '+') && ft_isdigit(nptr[i + 1]))
+		|| ft_isdigit(nptr[i]))
 	{
-		if (nptr[i] == '-')
-		{
+		if (nptr[i++] == '-')
 			signal *= -1;
-			i++;
-		}
 		while (nptr[i] >= '0' && nptr[i] <= '9')
 		{
 			number = (nptr[i] - '0') + (number * 10);
