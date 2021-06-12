58671a4ef1882269870d14ac51c6519fb5a9e07f
Rods <rodrigogregoldo@gmail.com>
2021-06-12 15:54:44 -0300

    ft_strmapi done maybe ???

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -2,7 +2,6 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	// vai precisar da itoa
 	// joga resultado itoa em ft_putstr_fd
 	ft_putstr_fd(ft_itoa(n), fd);
 }
--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -1,4 +1,20 @@
-// char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-// {
-// 	
-// }
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	length;
+	char	*result;
+
+	if (s == NULL)
+		return (NULL);
+	length = ft_strlen(s);
+	result = malloc((length + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	while (length > 0)
+	{
+		result[length - 1] = f(length - 1, s[length - 1]);
+		length--;
+	}
+	return (result);
+}
