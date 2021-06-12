a30ecee83fd8d86b584d2afc57852d4db54ce5bb
Rods <rodrigogregoldo@gmail.com>
2021-06-12 11:54:34 -0300

    ft_atoi DONE and TESTED

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -11,16 +11,13 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ')
 		i++;
-	if (((nptr[i] == '-' || nptr[i] == '+') && ft_isdigit(nptr[i + 1]))
-		|| ft_isdigit(nptr[i]))
-	{
+	if ((nptr[i] == '-' || nptr[i] == '+'))
 		if (nptr[i++] == '-')
 			signal *= -1;
-		while (nptr[i] >= '0' && nptr[i] <= '9')
-		{
-			number = (nptr[i] - '0') + (number * 10);
-			i++;
-		}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		number = (nptr[i] - '0') + (number * 10);
+		i++;
 	}
 	return (number * signal);
 }
