5fdf2eeaaab28403b3aafc203b706f54b2ca6d83
Rods <rodrigogregoldo@gmail.com>
2021-06-13 17:24:12 -0300

    ft_split DONE hopefully???

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -1,31 +1,88 @@
 #include "libft.h"
+// a string original pode ter n vezes o delimitador
+// se a string tiver só ' 's e o delimitador for ' ', o retorno deve ser um ponteiro vazio (strdup("");)
+// o delimitador é descartado sempre
+// se todos os caracteres forem iguais ao delimitador, então retorno strdup("");
+// se for "banana         abacate" e delimitador = ' '; o resultado é 2 strings: "banana" e "abacate";
 
-size_t ft_strlen(const char *s);
-
-char	**ft_split(char const *s, char c)
+// [OK] 1. função que conta qtos delimitadores tem (vai dizer qtos strings existirão no final)
+static size_t	count_delimiters(char const *s, char delimiter)
 {
-	char	**halves;
-	int		cleft;
-	int		i;
+	size_t	count;
 
-	halves = malloc(2 * sizeof(*halves));
-	cleft = 0;
-	while (s[cleft] != c)
-		cleft++;
-	halves[0] = malloc(cleft * sizeof(char) + 1);
-	halves[1] = malloc((ft_strlen(s) - cleft) * sizeof(char) + 1);
-	if (halves[0] == NULL || halves[1] == NULL)
-		return (NULL);
-	halves[0][cleft] = '\0';
-	halves[1][ft_strlen(s) - cleft] = '\0';
-	i = 0;
-	while (s[i])
+	count = 0;
+	while(*s)
 	{
-		if (i < cleft)
-			halves[0][i] = s[i];
-		else
-			halves[1][i - cleft] = s[i];
-		i++;
+		if (*s == delimiter)
+			count++;
+		s++;
 	}
-	return (halves);
+	return (count);
+}
+
+// [OK] 2. conta quantas palavras (válidas) existem
+static size_t	count_words(char const *s, char delimiter)
+{
+	size_t	words;
+
+	words = 0;
+	while(ft_strchr(s, delimiter) != NULL)
+	{
+		words++;
+		s = ft_strchr(s, delimiter);
+		while (*s == delimiter)
+			s++;
+	}
+	if (*s != delimiter && *s)
+		words++;
+	return (words);
+}
+
+// 3. função que alloca e cria o array de uma palavra por vez 
+static char		*get_new_word(char const *ptr, char delimiter)
+{
+	size_t	length;
+	char	*word;
+
+	length = 0;
+	while (ptr[length] != delimiter && ptr[length])
+		length++;
+	word = ft_calloc(length + 1, sizeof(char));
+	if (word == NULL)
+		return (NULL);
+	word[length] = '\0';
+	while (length > 0)
+	{
+		word[length - 1] = ptr[length - 1];
+		length--;
+	}
+	return (word);
+}
+
+char			**ft_split(char const *s, char c)
+{
+	char	**morsels;
+	size_t	slength;
+	size_t	delim_count;
+	size_t	word_count;
+	size_t	i;
+
+	slength = ft_strlen(s);
+	delim_count = count_delimiters(s, c);
+	word_count = count_words(s, c);
+	if (word_count == 0 || slength == delim_count)
+		return (NULL);
+	morsels = malloc((word_count + 1) * sizeof(*morsels));
+	if (morsels == NULL)
+		return (NULL);
+	i = 0;
+	while (++i <= word_count)
+	{
+		morsels[0] = get_new_word(s, c);
+		s = ft_strchr(s, c);
+		while (*s == c)
+			s++;
+		morsels++;
+	}
+	return (morsels - word_count);
 }
