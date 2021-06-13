61b32df94028d83f07b3d12c7298c8be163c85f7
Rods <rodrigogregoldo@gmail.com>
2021-06-13 17:45:38 -0300

    strnstr fix where a line was deleted when cleaning comments

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -5,12 +5,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	unsigned int i;
 	unsigned int j;
 
-	i = 0;
 	if (*needle == '\0')
 		return ((char *) haystack);
+	i = 0;
 	while (haystack[i] && i < len)
 	{
 		j = 0;
+		while (haystack[i + j] == needle[j] && (i + j) < len)
 		{
 			j++;
 			if (needle[j] == '\0')
