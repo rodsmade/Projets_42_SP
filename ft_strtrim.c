6364d62c5dfb294372dfde931e71f82bb85dcb1e
Rods <rodrigogregoldo@gmail.com>
2021-06-16 22:07:12 -0300

    ft_strtrim OKKKK

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -9,7 +9,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (ft_strchr(set, *s1) && *s1)
 		s1++;
 	i = ft_strlen(s1);
-	while (ft_strchr(set, s1[i]) && i)
+	while (ft_strchr(set, s1[i - 1]) && i)
 		i--;
-	return (ft_substr(s1, 0, i + 1));
+	return (ft_substr(s1, 0, i));
 }
