4c0c8954896a26cbbcec7ec5d9b75c0cb769a7a4
Rods <rodrigogregoldo@gmail.com>
2021-06-16 21:31:39 -0300

    ft_substr okkkkk

--- ft_substr.c
+++ ft_substr.c
@@ -2,28 +2,22 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char			*p;
-	unsigned int	i;
-	unsigned int	chars_left_in_s;
+	char	*substring;
+	size_t	slength;
+	size_t	i;
 
-	chars_left_in_s = ft_strlen(s) - start;
-	if (ft_strlen(s) < start)
+	slength = ft_strlen(s);
+	if (start >= slength)
 		return (ft_strdup(""));
-	p = malloc((len + 1) * sizeof(char));
-	if (p == NULL)
-	{
-		free (p);
-		return (NULL);
-	}
+	if (slength - start < len)
+		len = slength - start;
+	substring = malloc((len + 1) * sizeof(char));
 	i = 0;
-	while (*s && i < len)
+	while (s[i + start] && (i < len))
 	{
-		p[i] = s[start + i];
+		substring[i] = s[start + i];
 		i++;
 	}
-	if (chars_left_in_s > len)
-		p[len] = '\0';
-	else
-		p[chars_left_in_s + 1] = '\0';
-	return (p);
+	substring[len] = '\0';
+	return (substring);
 }
