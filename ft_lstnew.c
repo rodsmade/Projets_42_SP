5e00f601c735bd22519f9f03b80b88bccd687e2f
Rods <rodrigogregoldo@gmail.com>
2021-06-19 16:55:41 -0300

    ft_lsnew done

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content);
+t_list	*ft_lstnew(void *content)
 // Parameters #1. The content to create the new element with.
 // Return value The new element.
 // External functs. malloc
@@ -8,3 +8,19 @@ t_list	*ft_lstnew(void *content);
 // element. The variable ’content’ is initialized
 // with the value of the parameter ’content’. The
 // variable ’next’ is initialized to NULL.
+{
+	t_list	*newitem;
+
+	newitem = malloc(sizeof(t_list));
+	newitem->content = content;
+	newitem->next = NULL;
+	return (newitem);
+}
+
+#include <stdio.h>
+int main()
+{
+	t_list *newitem = ft_lstnew((void *) "Hello world?");
+	printf("%s\n", (char *) newitem->content);
+	return (0);
+}
