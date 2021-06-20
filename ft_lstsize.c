1cf8d57ad9d6c2234d2e1633e94a2f258330f463
Rods <rodrigogregoldo@gmail.com>
2021-06-20 17:20:55 -0300

    ft_lstsize done and tested

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -8,7 +8,9 @@ int	ft_lstsize(t_list *lst)
 {
 	int	count_items;
 
-	count_items = 0;
+	if (lst == NULL)
+		return (0);
+	count_items = 1;
 	while(lst->next != NULL)
 	{
 		count_items++;
@@ -16,3 +18,26 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (count_items);
 }
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	// t_list	**list_beginning;
+// 	t_list	*ptr_head;
+// 	// list_beginning = &ptr_head;
+// 	t_list	element1;
+// 	ptr_head = &element1;
+// 	t_list	element2;
+// 	t_list	element3;
+// 	// t_list	element4;
+// 	element1.content = (void *) "im the first element of the list";
+// 	element1.next = &element2;
+// 	element2.content = (void *) "im the second element of the list";
+// 	element2.next = &element3;
+// 	element3.content = (void *) "im the third element of the list";
+// 	element3.next = NULL;
+// 	// element4.content = (void *) "fourth element of the list";
+// 	// element4.next = NULL;
+// 	printf("tamanho da lista: %i\n", ft_lstsize(ptr_head));
+// 	return (0);
+// }
