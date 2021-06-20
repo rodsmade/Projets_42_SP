0ed1cbb9d8e3659fc5c708d0859db71dfc8210c4
Rods <rodrigogregoldo@gmail.com>
2021-06-20 20:20:29 -0300

    ft_lstiter done

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 // Parameters #1. The adress of a pointer to an element.
 // #2. The adress of the function used to iterate on
 // the list.
@@ -8,3 +8,13 @@ void	ft_lstiter(t_list *lst, void (*f)(void *));
 // External functs. None
 // Description Iterates the list ’lst’ and applies the function
 // ’f’ to the content of each element.
+{
+	if (lst == NULL)
+		return;
+	f(lst->content);
+	while (lst->next)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
