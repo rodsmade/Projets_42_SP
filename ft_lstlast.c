9016d3cff19e4ab5178e02bcdb5c3f47e11595f4
Rods <rodrigogregoldo@gmail.com>
2021-06-20 17:33:09 -0300

    ft_lstlast done and tested

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,30 @@ t_list	*ft_lstlast(t_list *lst)
 // External functs. None
 // Description Returns the last element of the list.
 {
+	if (lst->next == NULL)
+		return lst;
 	while (lst->next != NULL)
 		lst++;
 	return (lst);
+}
+
+#include <stdio.h>
+int main(void)
+{
+	t_list	*ptr_head;
+	t_list	element1;
+	// t_list	element2;
+	// t_list	element3;
+	// t_list	element4;
+	ptr_head = &element1;
+	element1.content = (void *) "im the first element of the list";
+	element1.next = NULL;
+	// element2.content = (void *) "im the second element of the list";
+	// element2.next = &element3;
+	// element3.content = (void *) "im the third element of the list";
+	// element3.next = &element4;
+	// element4.content = (void *) "fourth element of the list";
+	// element4.next = NULL;
+	printf("último elemento da lista: %s\n", (char* ) ft_lstlast(ptr_head)->content);
+	return (0);
 }
