9dc96b6ca8c089eada65848b12be25ed57ff2f7f
Rods <rodrigogregoldo@gmail.com>
2021-09-17 12:32:51 -0300

    OMG I DID IT  YAAAAAAAAAAASSSSS

--- projets_obligatoires/2_get_next_line/get_next_line.h
+++ projets_obligatoires/2_get_next_line/get_next_line.h
@@ -8,6 +8,7 @@ char	*get_next_line(int fd);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 
 # endif
