59c298cdce75c57d1b4cf1a697d1996611e4253b
Rods <rodrigogregoldo@gmail.com>
2021-09-17 17:13:25 -0300

    all norminette checked except for line excess on gnl

--- projets_obligatoires/2_get_next_line/get_next_line.c
+++ projets_obligatoires/2_get_next_line/get_next_line.c
@@ -3,14 +3,14 @@
 static int	contains_nl(char *string)
 // true if return >= 0; false otherwise
 {
-	int i;
+	int	i;
 
-	if(!string)
+	if (!string)
 		return (-1);
 	i = 0;
-	while(string[i])
+	while (string[i])
 	{
-		if(string[i] == '\n')
+		if (string[i] == '\n')
 			return (i);
 		i++;
 	}
@@ -19,7 +19,7 @@ static int	contains_nl(char *string)
 
 static void	*free_n_quit(char *string)
 {
-	if(string)
+	if (string)
 		free(string);
 	return (NULL);
 }
@@ -29,10 +29,10 @@ static char	*copy_up_to_nl(char *string)
 	char	*copy;
 	int		nl_pos;
 
-	nl_pos = 0;
-	if(!string)	
+	nl_pos = contains_nl(string);
+	if (!string)
 		return (NULL);
-	if(!((nl_pos = contains_nl(string)) >= 0))
+	if (!(nl_pos >= 0))
 		return (string);
 	copy = malloc((nl_pos + 2) * sizeof(char));
 	if (copy == NULL)
@@ -49,19 +49,19 @@ static char	*save_past_first_nl(char *source)
 	int		nl_pos;
 	int		strlen;
 
-	if(!((nl_pos = contains_nl(source)) >= 0))
+	nl_pos = contains_nl(source);
+	if (!(nl_pos >= 0))
 		return (NULL);
 	dest = malloc(((strlen = ft_strlen(source)) - nl_pos) * sizeof(char));
-	if(dest == NULL)
+	if (dest == NULL)
 		return (NULL);
 	dest[strlen - nl_pos - 1] = '\0';
 	ft_strlcpy(dest, source + nl_pos + 1, strlen - nl_pos);
 	return (dest);
 }
 
-char		*get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	// não preciso checar se tem ou se não tem algo no resto. parto da premissa de que tem, e vou dando strjoin.
 	static char	*rest;
 	char		*buffer;
 	char		*line;
@@ -74,10 +74,10 @@ char		*get_next_line(int fd)
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (buffer == NULL)
 		return (NULL);
-	while(chars_read == BUFFER_SIZE && contains_nl(line) < 0)
+	while (chars_read == BUFFER_SIZE && contains_nl(line) < 0)
 	{
 		chars_read = read(fd, buffer, BUFFER_SIZE);
-		if(chars_read < 0)
+		if (chars_read < 0)
 		{
 			free(rest);
 			rest = NULL;
--- projets_obligatoires/2_get_next_line/get_next_line.h
+++ projets_obligatoires/2_get_next_line/get_next_line.h
@@ -11,4 +11,4 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 
-# endif
+#endif
