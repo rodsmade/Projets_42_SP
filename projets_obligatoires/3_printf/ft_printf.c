da7f61df04e762794c101bf0745dcfe3fd13c6b3
Rods <rodrigogregoldo@gmail.com>
2021-09-23 19:56:24 -0300

    okay i kinda know now how to work with va lists but.....

--- projets_obligatoires/3_printf/ft_printf.c
+++ projets_obligatoires/3_printf/ft_printf.c
@@ -1,38 +1,49 @@
 #include <stdio.h>
+#include <stdarg.h>
 
 int	ft_printf(const char *formatString, ...)
 {
 	unsigned int	write_count;
+	va_list			args_list;
 
+	// faz a lista apontar pro primeiro argumento da função;
+	// o seja: inicializa args_list;
+	va_start(args_list, formatString);
 	write_count = 0;
 	while (*formatString)
 	{
-		if (*formatString == '%')
-			write_count += convert_format(formatString, write_count);
-		else
+		if (*formatString != '%')
 			write_count += write(1, formatString, 1);
+		else
+		{
+			// eu acho que tenho que passar a lista ou o endereço da lista pra dentro dessa de conversão
+			write_count += convert_format(formatString, write_count);
+		}
 		formatString++;
 	}
+	va_end(args_list);
+	return (write_count);
 }
 
 unsigned int	convert_format(const char *formatString, unsigned int count)
 {
-	if (*format_string == 'c')
-		print_char(formatString, count);
-	if (*format_string == 's')
-		print_string(formatString, count);
-	if (*format_string == 'p')
-		print_pointer(formatString, count);
-	if (*format_string == 'd')
-		print_double(formatString, count);
-	if (*format_string == 'i')
-		print_int(formatString, count);
-	if (*format_string == 'u')
-		print_udecimal(formatString, count);
-	if (*format_string == 'x')
-		print_uhex_lowercase(formatString, count);
-	if (*format_string == 'X')
-		print_uhex_uppercase(formatString, count);
-	if (*format_string == '%')
-		print_percent_sign(formatString, count);
+	if (*formatString == 'c')
+		count += print_char(formatString, count);
+	if (*formatString == 's')
+		count += print_string(formatString, count);
+	if (*formatString == 'p')
+		count += print_pointer(formatString, count);
+	if (*formatString == 'd')
+		count += print_double(formatString, count);
+	if (*formatString == 'i')
+		count += print_int(formatString, count);
+	if (*formatString == 'u')
+		count += print_udecimal(formatString, count);
+	if (*formatString == 'x')
+		count += print_uhex_lowercase(formatString, count);
+	if (*formatString == 'X')
+		count += print_uhex_uppercase(formatString, count);
+	if (*formatString == '%')
+		count += print_percent_sign(formatString, count);
+	return (count);
 }
