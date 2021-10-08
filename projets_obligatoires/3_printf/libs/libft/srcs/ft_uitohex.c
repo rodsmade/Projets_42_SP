cf3924a88ccf6d0f244b68598e5551a79f84ed2f
Rods <rodrigogregoldo@gmail.com>
2021-10-08 20:27:06 -0300

    digit count fixed

--- projets_obligatoires/3_printf/libs/libft/srcs/ft_uitohex.c
+++ projets_obligatoires/3_printf/libs/libft/srcs/ft_uitohex.c
@@ -6,31 +6,44 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 10:54:21 by roaraujo          #+#    #+#             */
-/*   Updated: 2021/10/08 20:07:57 by roaraujo         ###   ########.fr       */
+/*   Updated: 2021/10/08 20:16:04 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	count_digits(size_t n, int base_length)
+{
+	unsigned int	digits;
+
+	if (n == 0)
+		return (1);
+	digits = 0;
+	while (n > 0)
+	{
+		digits++;
+		n /= base_length;
+	}
+	return (digits);
+}
+
 char	*ft_uitohex(unsigned int n, char *base_str)
 {
 	unsigned int	int_to_hex;
 	size_t			digits;
-	int				base;
+	int				base_length;
 	char			*result;
 
-	base = ft_strlen(base_str);
+	base_length = ft_strlen(base_str);
 	int_to_hex = n;
-	digits = 1;
-	while (n /= base)
-		digits++;
+	digits = count_digits(n, base_length);
 	if (!(result = (char *)malloc(digits + 1)))
 		return (0);
 	result[digits--] = '\0';
 	while (int_to_hex > 0)
 	{
-		result[digits--] = base_str[int_to_hex % base];
-		int_to_hex /= base;
+		result[digits--] = base_str[int_to_hex % base_length];
+		int_to_hex /= base_length;
 	}
 	if (digits == 0 && result[1] == '\0')
 		result[0] = '0';
