1066dbceb253ebb37a5be167d0ee2eb8f9546010
rodsmade <rodrigogregoldo@gmail.com>
2021-12-05 01:12:38 +0100

    Pikachu is finally bound by boulders

--- projets_obligatoires/5_so_long/srcs/movement_utils.c
+++ projets_obligatoires/5_so_long/srcs/movement_utils.c
@@ -6,7 +6,7 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 23:14:54 by roaraujo          #+#    #+#             */
-/*   Updated: 2021/12/05 00:51:57 by roaraujo         ###   ########.fr       */
+/*   Updated: 2021/12/05 01:01:25 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,31 @@
 void	move_left(t_game *game)
 {
 	game->player->direction = 'j';
-	game->player->pos->y--;
-	// if (game->map->map_2D[game->player->pos->y][game->player->pos->y - 1] != '1')
-	// 	game->player->pos->y--;
-	// if (game->player->pos->x - game->map->tile_width >= 0)
-	// 	game->player->pos->x -= game->map->tile_width;
+	if (game->map->map_2D[game->player->pos->x][game->player->pos->y - 1] != '1')
+		game->player->pos->y--;
 	return ;
 }
 
 void	move_up(t_game *game)
 {
 	game->player->direction = 'i';
-	game->player->pos->x--;
-	// if (game->player->pos->y - game->map->tile_height >= 0)
-	// 	game->player->pos->y -= game->map->tile_height;
+	if (game->map->map_2D[game->player->pos->x - 1][game->player->pos->y] != '1')
+		game->player->pos->x--;
 	return ;
 }
 
 void	move_right(t_game *game)
 {
 	game->player->direction = 'l';
-	game->player->pos->y++;
-	// if (game->player->pos->x + game->map->tile_width <= game->window->width - game->map->tile_width)
-	// 	game->player->pos->x += game->map->tile_width;
+	if (game->map->map_2D[game->player->pos->x][game->player->pos->y + 1] != '1')
+		game->player->pos->y++;
 	return ;
 }
 
 void	move_down(t_game *game)
 {
 	game->player->direction = 'k';
-	game->player->pos->x++;
-	// if (game->player->pos->y + game->map->tile_height <= game->window->height - game->map->tile_height)
-	// 	game->player->pos->y += game->map->tile_height;
+	if (game->map->map_2D[game->player->pos->x + 1][game->player->pos->y] != '1')
+		game->player->pos->x++;
 	return ;
 }
