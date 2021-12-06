e9fad5593d41b278d59e5a000f4ab222bc9bb547
rodsmade <rodrigogregoldo@gmail.com>
2021-12-05 21:38:18 -0300

    fixing movement over exit tile

--- projets_obligatoires/5_so_long/srcs/movement_utils.c
+++ projets_obligatoires/5_so_long/srcs/movement_utils.c
@@ -15,6 +15,12 @@
 void	move_left(t_game *game)
 {
 	game->player->direction = 'j';
+	if (game->map->map_2D[game->player->pos->x][game->player->pos->y - 1] != 'E'
+		&& game->player->collec_count == game->map->C_count)
+	{
+		game->player->pos->y--;
+		render_you_won(game);
+	}
 	if (game->map->map_2D[game->player->pos->x][game->player->pos->y - 1] != '1')
 		game->player->pos->y--;
 	if (game->map->map_2D[game->player->pos->x][game->player->pos->y] == 'C')
@@ -22,17 +28,18 @@ void	move_left(t_game *game)
 		game->map->map_2D[game->player->pos->x][game->player->pos->y] = '0';
 		game->player->collec_count++;
 	}
-	if (game->map->map_2D[game->player->pos->x][game->player->pos->y] == 'E'
-		&& game->player->collec_count == game->map->C_count)
-	{
-		render_you_won(game);
-	}
 	return ;
 }
 
 void	move_up(t_game *game)
 {
 	game->player->direction = 'i';
+	if (game->map->map_2D[game->player->pos->x - 1][game->player->pos->y] != 'E'
+			&& game->player->collec_count == game->map->C_count)
+	{
+		game->player->pos->x--;
+		render_you_won(game);
+	}
 	if (game->map->map_2D[game->player->pos->x - 1][game->player->pos->y] != '1')
 		game->player->pos->x--;
 	if (game->map->map_2D[game->player->pos->x][game->player->pos->y] == 'C')
@@ -40,17 +47,18 @@ void	move_up(t_game *game)
 		game->map->map_2D[game->player->pos->x][game->player->pos->y] = '0';
 		game->player->collec_count++;
 	}
-	if (game->map->map_2D[game->player->pos->x][game->player->pos->y] == 'E'
-		&& game->player->collec_count == game->map->C_count)
-	{
-		render_you_won(game);
-	}
 	return ;
 }
 
 void	move_right(t_game *game)
 {
 	game->player->direction = 'l';
+	if (game->map->map_2D[game->player->pos->x][game->player->pos->y + 1] != 'E'
+			&& game->player->collec_count == game->map->C_count)
+	{
+		game->player->pos->y++;
+		render_you_won(game);
+	}
 	if (game->map->map_2D[game->player->pos->x][game->player->pos->y + 1] != '1')
 		game->player->pos->y++;
 	if (game->map->map_2D[game->player->pos->x][game->player->pos->y] == 'C')
@@ -58,28 +66,24 @@ void	move_right(t_game *game)
 		game->map->map_2D[game->player->pos->x][game->player->pos->y] = '0';
 		game->player->collec_count++;
 	}
-	if (game->map->map_2D[game->player->pos->x][game->player->pos->y] == 'E'
-		&& game->player->collec_count == game->map->C_count)
-	{
-		render_you_won(game);
-	}
 	return ;
 }
 
 void	move_down(t_game *game)
 {
 	game->player->direction = 'k';
+	if (game->map->map_2D[game->player->pos->x + 1][game->player->pos->y] != 'E'
+			&& game->player->collec_count == game->map->C_count)
+	{
+		game->player->pos->x++;
+		render_you_won(game);
+	}
 	if (game->map->map_2D[game->player->pos->x + 1][game->player->pos->y] != '1')
 		game->player->pos->x++;
 	if (game->map->map_2D[game->player->pos->x][game->player->pos->y] == 'C')
 	{
 		game->map->map_2D[game->player->pos->x][game->player->pos->y] = '0';
 		game->player->collec_count++;
-	}
-	if (game->map->map_2D[game->player->pos->x][game->player->pos->y] == 'E'
-		&& game->player->collec_count == game->map->C_count)
-	{
-		render_you_won(game);
 	}
 	return ;
 }
