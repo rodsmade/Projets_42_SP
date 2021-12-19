44772bf8849fb6b9686a79655f004f9a47c2f7fc
rodsmade <rodrigogregoldo@gmail.com>
2021-12-20 00:07:05 +0100

    simple pipe() example

--- 6_pipex/main.c
+++ 6_pipex/main.c
@@ -1,9 +1,9 @@
-// fork tests
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
 
-
+/*
 int main()
 {
 	pid_t parent_pid;
@@ -15,14 +15,14 @@ int main()
 	child_pid = fork();
 	if (child_pid < 0)
 		perror("Fork failed\n");
-	if (child_pid > 0)
+	else if (child_pid > 0)
 	{
 		printf("This is the parent process whereupon a fork returns a positive pid which is it's child's pid\n");
 		printf("parent_pid: %d\n", parent_pid);
 		printf("child_pid: %d\n", child_pid);
 		printf("getpid(): %d\n", getpid());
 	}
-	if (child_pid == 0)
+	else
 	{
 		printf("it means i forked inside a child process, whereupon a fork returns 0\n");
 		printf("parent_pid: %d\n", parent_pid);
@@ -37,18 +37,43 @@ int main()
 	printf("end of program, right before return\n");
 	return 0;
 }
+*/
 
-
-/*
+// LEARNING TO PIPE PROPERLY
 int main()
 {
-	printf("I am: %d\n", (int) getpid());
+	int		pipefd[2];
+	int		transfer;
+	pid_t	pid;
 
-	pid_t pid = fork();
-	printf("fork returned: %d\n", (int) pid);
-
-	printf("I am: %d\n", (int) getpid());
-
+	// from manual: pipefd[0] refers to the read end of the pipe.
+	// pipefd[1] refers to the write  end  of  the  pipe.
+	if (pipe(pipefd) == -1)
+	{
+		printf("An error ocurred while piping a a a\n");
+		return (0);
+	}
+	pid = fork();
+	if (pid < 0)
+	{
+		printf("An error ocurred while forking :O\n");
+		return (0);
+	}
+	else if (pid == 0)
+	{
+		close(pipefd[0]);
+		transfer = -42000;
+		write(pipefd[1], &transfer, sizeof(int));
+		close(pipefd[1]);
+		return (0);
+	}
+	else
+	{
+		close(pipefd[1]);
+		int	buffer;
+		wait(NULL);
+		read(pipefd[0], &buffer, sizeof(int));
+		printf("number read from pipefd: %d\n", buffer);
+	}
 	return (0);
 }
-*/
