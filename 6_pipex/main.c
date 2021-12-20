941a404a3cbb1f2681cacdd93a44ebb7187420a9
rodsmade <rodrigogregoldo@gmail.com>
2021-12-20 02:07:57 +0100

    implemented a very basic multithread i guess

--- 6_pipex/main.c
+++ 6_pipex/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 00:57:47 by roaraujo          #+#    #+#             */
+/*   Updated: 2021/12/20 02:06:57 by roaraujo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -39,6 +51,7 @@ int main()
 }
 */
 
+/*
 // LEARNING TO PIPE PROPERLY
 int main()
 {
@@ -77,3 +90,53 @@ int main()
 	}
 	return (0);
 }
+*/
+
+// USING PIPE TO SPLIT THE JOB BETWEEN TWO PROCESSES - NEXT STEP IS SPLITTING BETWEEN THREE (1 PAR 2 CHIL)
+int main()
+{
+	int		myArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int		pipefd[2];
+	int		i;
+	int		sum;
+	pid_t	pid;
+
+	if (pipe(pipefd) == -1)
+		return (-1);
+	pid = fork();
+	if (pid == -1)
+		return (-2);
+	else if (pid == 0)
+	{
+		close(pipefd[0]);
+		//adds up first half of array
+		sum = 0;
+		i = -1;
+		while (++i < 5)
+			sum += myArray[i];
+		printf("Partial sum: %d\n", sum);
+		// puts in pipe
+		write(pipefd[1], &sum, sizeof(int));
+		close(pipefd[1]);
+		return (0);
+	}	
+	else
+	{
+		close(pipefd[1]);
+		// gets first half sum from pipe
+		wait(NULL);
+		read(pipefd[0], &sum, sizeof(int));
+		printf("Sum from child: %d\n", sum);
+		//adds up second half of array
+		i = 4;
+		while (++i < 10)
+			sum += myArray[i];
+		printf("Total sum: %d\n", sum);
+		close(pipefd[0]);
+	}
+
+	return 0;
+}
+
+//TODO: exercício 1: rescrever esse trem daquele jeito que faz o double diamond kkkk n sei explica
+//TODO: exercício 2: e se fossem 3 processos ???
---
+++ 6_pipex/src/pipex.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 01:15:18 by roaraujo          #+#    #+#             */
+/*   Updated: 2021/12/20 01:16:06 by roaraujo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * ./pipex file1 cmd1 cmd2 file2
+ * MUST CORRESPOND TO
+ * < file1 cmd1 | cmd2 > file2
+ * */
+int	main(int argc, char *argv[], char *envp[])
+{
+	// diacho como entender essa entrada a a a :'(
+	// quantos args de entrada tem q ter no minimo? no máximo?
+}
