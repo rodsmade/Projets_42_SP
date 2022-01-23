329f76a0a480dae0ba728d1d137b45b85ff33e92
Rods <rodrigogregoldo@gmail.com>
2022-01-22 22:16:51 -0300

    renaming utils

--- 7_push_swap/include/push_swap.h
+++ 7_push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 18:15:13 by roaraujo          #+#    #+#             */
-/*   Updated: 2022/01/22 16:00:29 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/01/22 22:16:41 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,31 +38,31 @@ typedef struct s_stacks
 }					t_stacks;
 
 // -- PROTOTYPES --------------------------------------------------------------|
-// double_list_utils_1.c
+// utils_double_list_1.c
 t_dbl_list_i	*ft_dbl_lst_i_new(int content);
 int				ft_dbl_lst_i_size(t_dbl_list_i *lst);
 t_dbl_list_i	*ft_dbl_lst_i_last(t_dbl_list_i *lst);
 t_dbl_list_i	*ft_dbl_lst_i_first(t_dbl_list_i *lst);
 
-// double_list_utils_2.c
+// utils_double_list_2.c
 void			ft_dbl_lst_i_add_back(t_dbl_list_i **lst, t_dbl_list_i *new);
 void			ft_dbl_lst_i_add_front(t_dbl_list_i **lst, t_dbl_list_i *new);
 
-// operations_utils_push.c
+// utils_operations_push.c
 void			push_a(t_stacks *stacks);
 void			push_b(t_stacks *stacks);
 
-// operations_utils_reverse_rotate.c
+// utils_operations_reverse_rotate.c
 void			reverse_rotate_a(t_stacks *stacks);
 void			reverse_rotate_b(t_stacks *stacks);
 void			reverse_rotate_ab(t_stacks *stacks);
 
-// operations_utils_rotate.c
+// utils_operations_rotate.c
 void			rotate_a(t_stacks *stacks);
 void			rotate_b(t_stacks *stacks);
 void			rotate_ab(t_stacks *stacks);
 
-// operations_utils_swap.c
+// utils_operations_swap.c
 void			swap_a(t_stacks *stacks);
 void			swap_b(t_stacks *stacks);
 void			swap_ab(t_stacks *stacks);
