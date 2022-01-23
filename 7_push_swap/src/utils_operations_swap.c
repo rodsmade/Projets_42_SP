10bcac64c10803bb45f5a3854df0716aa5a4b745
Rods <rodrigogregoldo@gmail.com>
2022-01-23 15:38:07 -0300

    replicated fix in b for swap_a and swap both

--- 7_push_swap/src/utils_operations_swap.c
+++ 7_push_swap/src/utils_operations_swap.c
@@ -6,7 +6,7 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 20:47:10 by roaraujo          #+#    #+#             */
-/*   Updated: 2022/01/23 14:48:06 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/01/23 15:37:09 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@ void	swap_a(t_stacks *stacks)
 {
 	t_dbl_list_i	*new_first;
 	t_dbl_list_i	*new_second;
+	t_dbl_list_i	*third;
 
 	if (ft_dbl_lst_i_size(stacks->stack_a) < 2)
 		return ;
 	new_first = stacks->stack_a->next;
 	new_second = stacks->stack_a;
+	third = stacks->stack_a->next->next;
 	stacks->stack_a = new_first;
-	new_second->prev = new_first;
 	new_first->prev = NULL;
-	new_second->next = new_first->next;
 	new_first->next = new_second;
+	new_second->prev = new_first;
+	new_second->next = third;
+	third->prev = new_second;
 	ft_putendl_fd("sa", 1);
 	return ;
 }
@@ -55,6 +58,7 @@ void	swap_ab(t_stacks *stacks)
 {
 	t_dbl_list_i	*new_first;
 	t_dbl_list_i	*new_second;
+	t_dbl_list_i	*third;
 
 	if (ft_dbl_lst_i_size(stacks->stack_a) < 2
 		&& ft_dbl_lst_i_size(stacks->stack_b) < 2)
@@ -63,21 +67,25 @@ void	swap_ab(t_stacks *stacks)
 	{
 		new_first = stacks->stack_a->next;
 		new_second = stacks->stack_a;
+		third = stacks->stack_a->next->next;
 		stacks->stack_a = new_first;
-		new_second->prev = new_first;
 		new_first->prev = NULL;
-		new_second->next = new_first->next;
 		new_first->next = new_second;
+		new_second->prev = new_first;
+		new_second->next = third;
+		third->prev = new_second;
 	}
 	if (ft_dbl_lst_i_size(stacks->stack_a) >= 2)
 	{
 		new_first = stacks->stack_b->next;
 		new_second = stacks->stack_b;
+		third = stacks->stack_b->next->next;
 		stacks->stack_b = new_first;
-		new_second->prev = new_first;
 		new_first->prev = NULL;
-		new_second->next = new_first->next;
 		new_first->next = new_second;
+		new_second->prev = new_first;
+		new_second->next = third;
+		third->prev = new_second;
 	}
 	ft_putendl_fd("ss", 1);
 	return ;
