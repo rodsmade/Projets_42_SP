f9b68b5d4744436094cbad7dd0024158d8d70201
Rods <rodrigogregoldo@gmail.com>
2022-01-31 19:24:43 -0300

    fixed swap utils and i compromised for swapping contents instead of pointers ):

--- 7_push_swap/src/utils_operations_swap.c
+++ 7_push_swap/src/utils_operations_swap.c
@@ -6,7 +6,7 @@
 /*   By: roaraujo <roaraujo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 20:47:10 by roaraujo          #+#    #+#             */
-/*   Updated: 2022/01/23 15:37:09 by roaraujo         ###   ########.fr       */
+/*   Updated: 2022/01/31 19:07:39 by roaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,78 +14,44 @@
 
 void	swap_a(t_stacks *stacks)
 {
-	t_dbl_list_i	*new_first;
-	t_dbl_list_i	*new_second;
-	t_dbl_list_i	*third;
+	int	aux;
 
-	if (ft_dbl_lst_i_size(stacks->stack_a) < 2)
-		return ;
-	new_first = stacks->stack_a->next;
-	new_second = stacks->stack_a;
-	third = stacks->stack_a->next->next;
-	stacks->stack_a = new_first;
-	new_first->prev = NULL;
-	new_first->next = new_second;
-	new_second->prev = new_first;
-	new_second->next = third;
-	third->prev = new_second;
+	aux = stacks->stack_a->content;
+	stacks->stack_a->content = stacks->stack_a->next->content;
+	stacks->stack_a->next->content = aux;
 	ft_putendl_fd("sa", 1);
 	return ;
 }
 
 void	swap_b(t_stacks *stacks)
 {
-	t_dbl_list_i	*new_first;
-	t_dbl_list_i	*new_second;
-	t_dbl_list_i	*third;
+	int	aux;
 
-	if (ft_dbl_lst_i_size(stacks->stack_b) < 2)
-		return ;
-	new_first = stacks->stack_b->next;
-	new_second = stacks->stack_b;
-	third = stacks->stack_b->next->next;
-	stacks->stack_b = new_first;
-	new_first->prev = NULL;
-	new_first->next = new_second;
-	new_second->prev = new_first;
-	new_second->next = third;
-	third->prev = new_second;
+	aux = stacks->stack_b->content;
+	stacks->stack_b->content = stacks->stack_b->next->content;
+	stacks->stack_b->next->content = aux;
 	ft_putendl_fd("sb", 1);
 	return ;
 }
 
 void	swap_ab(t_stacks *stacks)
 {
-	t_dbl_list_i	*new_first;
-	t_dbl_list_i	*new_second;
-	t_dbl_list_i	*third;
+	int	aux;
 
 	if (ft_dbl_lst_i_size(stacks->stack_a) < 2
 		&& ft_dbl_lst_i_size(stacks->stack_b) < 2)
 		return ;
 	if (ft_dbl_lst_i_size(stacks->stack_a) >= 2)
 	{
-		new_first = stacks->stack_a->next;
-		new_second = stacks->stack_a;
-		third = stacks->stack_a->next->next;
-		stacks->stack_a = new_first;
-		new_first->prev = NULL;
-		new_first->next = new_second;
-		new_second->prev = new_first;
-		new_second->next = third;
-		third->prev = new_second;
+		aux = stacks->stack_a->content;
+		stacks->stack_a->content = stacks->stack_a->next->content;
+		stacks->stack_a->next->content = aux;
 	}
-	if (ft_dbl_lst_i_size(stacks->stack_a) >= 2)
+	if (ft_dbl_lst_i_size(stacks->stack_b) >= 2)
 	{
-		new_first = stacks->stack_b->next;
-		new_second = stacks->stack_b;
-		third = stacks->stack_b->next->next;
-		stacks->stack_b = new_first;
-		new_first->prev = NULL;
-		new_first->next = new_second;
-		new_second->prev = new_first;
-		new_second->next = third;
-		third->prev = new_second;
+		aux = stacks->stack_b->content;
+		stacks->stack_b->content = stacks->stack_b->next->content;
+		stacks->stack_b->next->content = aux;
 	}
 	ft_putendl_fd("ss", 1);
 	return ;
